2df9ec0d0c1fd2f8c16358a31a1d3afb225f79db
Vodoley89 <116215706+Vodoley89@users.noreply.github.com>
2022-12-13 22:41:56 +0300

    work_files

--- ScriptsDB/General/Scripts/Select.sql
+++ ScriptsDB/General/Scripts/Select.sql
@@ -1,22 +1,22 @@
 select  name_a, year_a from album_list 
 where year_a = 2018;
 
-select max ( duration) from track_list 
-;
-
+select max(duration), name_t   from track_list
+group by name_t 
+order by max(duration) desc ;
 
 
 select name_t,  duration from track_list
 where  duration <= 210;
 
 select  name_a, year_a from album_list 
-where year_a >= 2018 and year_a <= 2020;
+where year_a between 2018 and 2020;
  
 select nickname from executer sdat_name 
 where nickname not like '% %';
 
 select name_t from track_list
-where  name_t like'%мой%' or name_t like'%my%';
+where  name_t ilike'%Мой%' or name_t ilike  '%my%';
 
 
 
