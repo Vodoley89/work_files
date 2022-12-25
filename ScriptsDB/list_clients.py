ee233abf7b4f1175e9c840232a84f93593877e02
Vodoley89 <116215706+Vodoley89@users.noreply.github.com>
2022-12-25 19:04:29 +0300

    open_files

--- ScriptsDB/list_clients.py
+++ ScriptsDB/list_clients.py
@@ -1,97 +1,135 @@
 import psycopg2
 
-    # Функция, создающая структуру БД (таблицы)
+
+def drop_table(cur, *table):
+    for t in table:
+        cur.execute(f"DROP TABLE {t}")
+
+
+# Функция, создающая структуру БД (таблицы)
 def create_db(cur):
-        cur.execute('''
+    cur.execute('''
             CREATE TABLE IF NOT EXISTS  base_client (
             id SERIAL PRIMARY KEY,
             first_name VARCHAR (30) NOT NULL,
             last_name VARCHAR (30) NOT NULL,
-            id_phone INT REFERENCES phone_client(id),
-            email VARCHAR (50) UNIQUE);
-                        ''')
-
-        cur.execute('''
+            email VARCHAR (50) UNIQUE,
+            phones INT 
+            );
+            
             CREATE TABLE IF NOT EXISTS  phone_client (
-            id SERIAL PRIMARY KEY,
-            phone_num INT);
-                        ''')
+            id_phone SERIAL PRIMARY KEY,
+            id_client INT REFERENCES base_client(id) ON DELETE CASCADE,
+            phone INT );   
+            ''')
 
-    # Функция, позволяющая добавить нового клиента
+
+# Функция, позволяющая добавить нового клиента
 def add_client(cur, first_name, last_name, email, phones=None):
-        cur.execute('''
-            INSERT INTO base_client (
-            first_name, last_name, email)
-            VALUES  (%s, %s, %s)
-            RETURNING id;
+    cur.execute('''
+            INSERT INTO base_client 
+            (first_name, last_name, email)
+            VALUES  (%s, %s, %s) ;
                         ''', (first_name, last_name, email))
 
 
-    # Функция, позволяющая добавить телефон для существующего клиента
-def add_phone(cur, phone_num):
-        cur.execute('''
-            INSERT INTO phone_client ( phone_num) VALUES ( %s);
-                        ''', (phone_num))
+    if phones:
+        cur.execute("""
+            INSERT INTO phone_client( phone)
+            VALUES ( %s);
+            """, ( phones,))
 
-    # Функция, позволяющая изменить данные о клиенте
-def update_client(cur, first_name):
+
+
+# Функция, позволяющая добавить телефон для существующего клиента
+def add_phone(cur, id_client, phone):
+    cur.execute('''
+            INSERT INTO phone_client(id_client, phone)
+            VALUES (%s, %s);
+            ''', (id_client, phone))
+
+
+# Функция, позволяющая изменить данные о клиенте
+def update_client(cur, id, first_name=None, last_name=None, email=None, phones=None):
+    if first_name != None:
         cur.execute('''
             UPDATE base_client 
-            SET first_name = %s
+            SET first_name = %s            
             WHERE id = %s;''',
-                    (first_name))
+                    (first_name, id))
 
-def update_client(cur ,last_name):
+    if last_name != None:
         cur.execute('''
            UPDATE base_client 
-           SET last_name = %s
+           SET last_name (%s) = values (%s) 
            WHERE id = %s;''',
-                (last_name))
+                    (last_name, id))
 
-def update_client(cur, email):
+    if email != None:
         cur.execute('''
            UPDATE base_client 
            SET  email= %s
            WHERE id = %s;''',
-                    (email))
+                    (email, id))
 
-def update_client(cur, phone_num):
-        cur.execute('''
-            UPDATE phone_client 
-            SET phone_num = %s
-            WHERE id = %s; 
-            ''', (phone_num))
+    if phones != None:
+        cur.execute("""
+                    UPDATE phone_client
+                    SET phone = %s
+                    WHERE id_client = %s
+                    """, (phones, id))
 
-    # Функция, позволяющая удалить телефон для существующего клиента
-def delete_phone(cur,  phone_num):
-        cur.execute('''
-            DELETE FROM phone_client
-            WHERE id = %s;
-                    ''',(phone_num))
 
-    # Функция, позволяющая удалить существующего клиента
+# Функция, позволяющая удалить телефон для существующего клиента
+def delete_phone(cur, id_phone):
+    cur.execute('''
+            DELETE FROM phone_client 
+            WHERE id_phone = %s;
+                    ''', ( id_phone,))
+
+
+# Функция, позволяющая удалить существующего клиента
 def delete_client(cur, id):
-        cur.execute('''
-            DELETE FROM base_client(id)
-            WHERE id = %s;
-        ''',(id))
+    cur.execute('''
+              DELETE FROM base_client
+              WHERE id = %s;
+              ''', (id,))
 
-    # Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону)
+             
+                       
+
+
+
+# Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону)
 def find_client(cur, first_name=None, last_name=None, email=None, phones=None):
-        cur.exrcute('''
-            SELECT first_name, last_name, email, phones 
-            FROM base_client
-            WHERE first_name = %s or last_name = %s or email = %s or phones = %s
-        ''', (first_name, last_name, email, phones))
+    cur.execute(''' 
+                SELECT * FROM base_client bc 
+                JOIN  phone_client pc
+                 ON pc.id_client = bc.id
+                WHERE first_name =%s or last_name =%s or email =%s or phone =%s;
+               ''', (first_name, last_name, email, phones))
+
+
+    return cur.fetchall()
+
+
 
 from config import conn
 with conn.cursor() as cur:
-     create_db(cur)
-     add_client(cur, 'Nikolai', 'Nik', 'Nik@m.ru', 12345678)
-     add_phone(cur, 12345678)
-     update_client(cur,'Nikolai', 1)
-     update_phone(cur, 1 , 87654321)
-     delete_phone(cur,1, 87654321)
-     delete_client(cur, '1')
-     find_client(cur, 'Nik')
-conn.commit
+    drop_table(cur , 'phone_client', 'base_client')
+    create_db(cur)
+    add_client(cur, 'Nikolai', 'Nik', 'Nik@m.ru')
+    add_client(cur, 'Vasilij', 'Vas', 'Vas@m.ru')
+    add_phone(cur, 1, 7777777)
+    add_phone(cur, 2, 8888888)
+    add_phone(cur,2, 9999999)
+    add_phone(cur,2, 5555555)
+    update_client(cur, first_name='Marina', id=1)
+    update_client(cur,email='nevasya@j.com', id=2)
+    delete_phone(cur, id_phone=4)
+    delete_client(cur, id=1)
+    find_client(cur, last_name='Nik')
+
+
+
+    conn.commit()
